7803a315e7c3e92ef0e2f4dc744a0bdcab553ffa
loopingstars <edsonvirginiodiniz.jr135@gmail.com>
2024-05-04 00:47:34 -0300

    Falta apenas criar o lógica de comparação e melhorar a questão dos arrray.

--- source/js/index.js
+++ source/js/index.js
@@ -5,8 +5,11 @@ var button = document.querySelector("#button")
 var Text = document.querySelectorAll(".Texti");
 var conteudoCSV = document.querySelector("#input");
 var audioSelect = document.querySelector("#audio-play");
+var modulo = document.querySelector('#Modulo');
 var Main = document.querySelector("#Main");
-var globalArray = [];
+var textArray = [];
+var AudioArray = [];
+var AudioTexto = [];
 ///////////////////////////////
 
 ///////////////////////////////
@@ -21,9 +24,22 @@ function read_CSV(file, callback) {
   reader.readAsText(file);
 }
 ///////////////////////////////
-
 ///////////////////////////////
-////Converter CSV
+////Juntar AUDIO + TEXTO 
+///////////////////////////////
+function AudioTextoArray() {
+  
+}
+///////////////////////////////
+////Popular array de audios
+///////////////////////////////
+function audio_arrays_populate(event){
+    for (const file of event.target.files) {
+      let text = String(file.webkitRelativePath);
+      AudioArray.push(text);
+    }
+    
+}
 ///////////////////////////////
 function Convert_CSV(conteudoCSV) {
   const lines = conteudoCSV.split('\n');
@@ -37,16 +53,15 @@ function Convert_CSV(conteudoCSV) {
   return arrayCSV;
 }
 ///////////////////////////////
-function ouvirVARS() {
-  Text = addEventListener("click", play_audio());
-}
+
 ///////////////////////////////
 ////Criar DIV
 ///////////////////////////////
 function CriarDIV(texto, number) {
-  let div = document.querySelector(".Texti");
-  Main.appendChild(div.cloneNode(true));
+  let variable = document.createElement('div');
+  variable.innerHTML = '<div class="Texti" onclick="TextoAction()"></div>';
   
+  Main.appendChild(variable);
 }
 ///////////////////////////////
 function alterarDIV(texto, number) {
@@ -58,30 +73,52 @@ function alterarDIV(texto, number) {
 ///////////////////////////////
 ////Iniciar audio
 ///////////////////////////////
-function play_audio(){
-  console.log("foi");
+function play_audio(Path){
+  audioSelect.setAttribute('src', Path);
   audioSelect.play();
 }
 ///////////////////////////////
-
+document.getElementById("filepicker").addEventListener(
+  "change",
+  (event) => {
+    for (const file of event.target.files) {
+      let text = String(file.webkitRelativePath);
+      AudioArray.push(text);
+    }
+  },
+  false,
+);
 
 input.addEventListener('change', function (event) {
   const arquive = event.target.files[0];
   read_CSV(arquive, function (content) {
-    globalArray = Convert_CSV(content);
+    textArray = Convert_CSV(content);
   })
 });
+///////////////////////////////
+////Texto Action
+///////////////////////////////
+function TextoAction() {
+  let Path = (modulo.options[modulo.selectedIndex].value + AudioArray[0]);
+  console.log(Path);
+  play_audio(Path)
+}
+///////////////////////////////
+///////////////////////////////
+////Comparar Arrays
+///////////////////////////////
 
+///////////////////////////////
 function iniciar() {
-  for (var i = 0; i < globalArray.length; i++) {
-    CriarDIV(globalArray[i][0], i);
+  for (var i = 0; i < textArray.length -1; i++) {
+    CriarDIV(textArray[i][0], i);
   }
-  for (var i = 0; i < globalArray.length; i++) {
-    alterarDIV(globalArray[i][0], i);
+  for (var i = 0; i < textArray.length -1; i++) {
+    alterarDIV(textArray[i][1], i);
+    
   }
 }
 
-//document.querySelector(".div-text").addEventListener("click", () => {play_audio()});
 button.addEventListener("click", () => {
   iniciar();
 });
--- teste/teste.js
+++ teste/teste.js
@@ -1,8 +1,32 @@
 var main = document.querySelector("#main");
 var my = document.createElement('button');
+var input = document.querySelector("#input");
+var modulo = document.querySelector('#Modulo');
+var aula = document.querySelector('#Aula');
+var audio = document.querySelector('#audio');
+
+var arrays = [];
 var clicar = document.querySelector("#clicar").addEventListener('click', () => {
-    main.innerHTML = '<button id="clicar2" class="minhaclasse">clicar2</button>';
-    
+    my.innerHTML = '<button id="clicar2" class="minhaclasse">clicar2</button>';
+    main.appendChild(my);
 });
 
+document.getElementById("filepicker").addEventListener(
+    "change",
+    (event) => {
+      for (const file of event.target.files) {
+        let text = String(file.webkitRelativePath);
+        arrays.push(text);
+      }
+    },
+    false,
+  );
 
+document.querySelector('#clicar').addEventListener('click', () => {
+    modulo = modulo.options[modulo.selectedIndex].value;
+    console.log(modulo);
+    console.log(arrays);
+    let Path = (modulo + arrays[1]);
+    audio.setAttribute('src', Path);
+    audio.play();
+} );
